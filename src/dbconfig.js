615a33bd4bddd162573afaa467779d834e8404e0
Kevin Medeiros <kevin.medeiros91@gmail.com>
2024-02-26 16:02:58 -0300

    wip

--- src/dbconfig.js
+++ src/dbconfig.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 // Função para criar e retornar uma nova instância do navegador com a flag --no-sandbox
 const createBrowserInstance = async () => {
   const browser = await puppeteer.launch({
+    headless: false,
     executablePath: "/usr/bin/chromium-browser",
     ignoreDefaultArgs: ["--disable-extensions"],
   });
