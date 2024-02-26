3c58dda78aebe5e41ae0e7d22b27d6c121ed8465
Kevin Medeiros <kevin.medeiros91@gmail.com>
2024-02-26 15:56:00 -0300

    wip

--- src/dbconfig.js
+++ src/dbconfig.js
@@ -8,9 +8,12 @@ const createBrowserInstance = async () => {
     executablePath: "/usr/bin/chromium-browser",
     args: [
       "--disable-gpu",
+      "--disable-dev-shm-usage",
       "--disable-setuid-sandbox",
+      "--no-first-run",
       "--no-sandbox",
       "--no-zygote",
+      "--single-process",
     ],
   });
   return browser;
