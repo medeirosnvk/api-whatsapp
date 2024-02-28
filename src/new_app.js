0950f31558a86198e7a929558c7f36f2d728deff
Kevin Medeiros <kevin.medeiros91@gmail.com>
2024-02-28 17:58:46 -0300

    wip

--- src/new_app.js
+++ src/new_app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const qrcode = require("qrcode-terminal");
+const { MessageMedia } = require("whatsapp-web.js");
 
 const { Client, LocalAuth } = require("whatsapp-web.js");
 
