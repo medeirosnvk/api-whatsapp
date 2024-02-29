82430cd8f6e93f151a3cf0de2d4cf189b536d03c
Kevin Medeiros <kevin.medeiros91@gmail.com>
2024-02-29 11:19:44 -0300

    wip

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: "npm", // Substitua pelo nome do seu arquivo de entrada do servidor Node.js
       args: "start", // Substitua pelo nome do seu arquivo de entrada do servidor Node.js
       watch: true, // Isso reiniciará o servidor quando houver alterações nos arquivos
-      cwd: "backend",
+      cwd: "src",
       env: {
         PORT: 3002, // Porta para o servidor Node.js
       },
