63379c59facac6d8c2a4b36b2d34793ebd008a7b
Kevin Medeiros <kevin.medeiros91@gmail.com>
2024-03-07 14:58:15 -0300

    ajuste porta 3030

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-const baseURL = "http://191.252.214.9:3000";
+const baseURL = "http://191.252.214.9:3030";
 // const baseURL = 'http://localhost:3000';
 
 const axiosApiInstance = axios.create({ baseURL });
