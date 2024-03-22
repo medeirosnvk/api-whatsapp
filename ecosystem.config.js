3eab961e57729a9f77ee59688d55a30568190da1
Kevin Medeiros <kevin.medeiros91@gmail.com>
2024-03-22 10:30:38 -0300

    ajuste ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,15 +10,5 @@ module.exports = {
         PORT: 3002,
       },
     },
-    {
-      name: "api-whatsapp-frontend",
-      script: "npm",
-      args: "run front",
-      watch: true,
-      cwd: "src/frontend",
-      env: {
-        PORT: 5175,
-      },
-    },
   ],
 };
