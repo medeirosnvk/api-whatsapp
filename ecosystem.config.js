3854001be9d32ad44bb161ff815e2235bccf51c4
Kevin Medeiros <kevin.medeiros91@gmail.com>
2024-04-18 11:16:29 -0300

    frontend e organizacao pastas

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,23 @@
 module.exports = {
   apps: [
     {
-      name: "api-whatsapp",
+      name: "app-whatsapp",
       script: "npm", // Substitua pelo nome do seu arquivo de entrada do servidor Node.js
       args: "start", // Substitua pelo nome do seu arquivo de entrada do servidor Node.js
       watch: true, // Isso reiniciará o servidor quando houver alterações nos arquivos
-      cwd: "src",
+      cwd: "app",
       env: {
         PORT: 3002, // Porta para o servidor Node.js
+      },
+    },
+    {
+      name: "app-whatsapp-frontend",
+      script: "yarn", // Use o gerenciador de pacotes (npm ou yarn)
+      args: "dev", // Comando para iniciar o aplicativo React
+      watch: true, // Isso reiniciará o servidor quando houver alterações nos arquivos
+      cwd: "frontend",
+      env: {
+        PORT: 5174, // Porta para o aplicativo React
       },
     },
   ],
