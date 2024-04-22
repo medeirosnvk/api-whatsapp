55929d267504b1486e6386d7e63d2567e26c0906
Kevin Medeiros <kevin.medeiros91@gmail.com>
2024-04-22 16:13:35 -0300

    wip

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,13 +6,14 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5174,
-  },
-  proxy: {
-    "/ws": {
-      target: "ws://191.101.70.186", // Altere para o endereço do seu servidor WebSocket
-      changeOrigin: true,
-      ws: true,
+    proxy: {
+      "/ws": {
+        target: "ws://191.101.70.186", // Altere para o endereço do seu servidor WebSocket
+        changeOrigin: true,
+        ws: true,
+      },
     },
+    host: "0.0.0.0", // Isso permitirá que o servidor seja acessível a partir de qualquer IP na rede local
+    port: 5174, // Altere para a porta desejada
   },
-  host: "0.0.0.0", // Isso permitirá que o servidor seja acessível a partir de qualquer IP
 });
