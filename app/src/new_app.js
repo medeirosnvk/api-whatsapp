4e91f81de873d2ac1ed5ac7c683c17355bb2a4ca
Kevin Medeiros <kevin.medeiros91@gmail.com>
2024-04-22 11:25:25 -0300

    wip

--- app/src/new_app.js
+++ app/src/new_app.js
@@ -552,299 +552,304 @@ class StateMachine {
   }
 
   async _handleOfertaState(origin, phoneNumber, response) {
-    if (response && response.body.trim().match(/^\d+$/)) {
-      const selectedOptionParcelamento = parseInt(response.body.trim());
-      const credorInfo = await requests.getCredorInfo(this.document);
-      const {
-        comissao_comercial,
-        idcomercial,
-        idgerente_comercial,
-        iddevedor,
-      } = credorInfo[0];
+    try {
+      if (response && response.body.trim().match(/^\d+$/)) {
+        const selectedOptionParcelamento = parseInt(response.body.trim());
+        const credorInfo = await requests.getCredorInfo(this.document);
+        const {
+          comissao_comercial,
+          idcomercial,
+          idgerente_comercial,
+          iddevedor,
+        } = credorInfo[0];
 
-      const credorOfertas = await requests.getCredorOfertas(this.idDevedor); // Acesse aqui
+        const credorOfertas = await requests.getCredorOfertas(this.idDevedor); // Acesse aqui
 
-      if (
-        selectedOptionParcelamento >= 1 &&
-        selectedOptionParcelamento <= credorOfertas.length
-      ) {
-        const ofertaSelecionada = credorOfertas[selectedOptionParcelamento - 1];
-        this._setDataOferta(phoneNumber, ofertaSelecionada);
-        console.log("ofertaSelecionada -", ofertaSelecionada);
+        if (
+          selectedOptionParcelamento >= 1 &&
+          selectedOptionParcelamento <= credorOfertas.length
+        ) {
+          const ofertaSelecionada =
+            credorOfertas[selectedOptionParcelamento - 1];
+          this._setDataOferta(phoneNumber, ofertaSelecionada);
+          console.log("ofertaSelecionada -", ofertaSelecionada);
 
-        const { periodicidade, valor_parcela, plano, idcredor, total_geral } =
-          ofertaSelecionada;
+          const { periodicidade, valor_parcela, plano, idcredor, total_geral } =
+            ofertaSelecionada;
 
-        const ultimaDataParcela = utils.getUltimaDataParcela(
-          periodicidade,
-          valor_parcela,
-          plano
-        );
+          const ultimaDataParcela = utils.getUltimaDataParcela(
+            periodicidade,
+            valor_parcela,
+            plano
+          );
 
-        const { parcelasArray, ultimaData } = ultimaDataParcela;
-        const ultimaDataFormat = ultimaData.toISOString().slice(0, 10);
+          const { parcelasArray, ultimaData } = ultimaDataParcela;
+          const ultimaDataFormat = ultimaData.toISOString().slice(0, 10);
 
-        const currentDate = new Date();
-        const currentTime = utils.getCurrentTime();
+          const currentDate = new Date();
+          const currentTime = utils.getCurrentTime();
 
-        const newDataBase =
-          currentDate.getDate() + parseInt(plano) * periodicidade;
-        const formattedDate = newDataBase.toString().substring(0, 10);
+          const newDataBase =
+            currentDate.getDate() + parseInt(plano) * periodicidade;
+          const formattedDate = newDataBase.toString().substring(0, 10);
 
-        const { data: promessas } = await requests.getCredorDividas(
-          this.idDevedor,
-          formattedDate
-        );
+          const { data: promessas } = await requests.getCredorDividas(
+            this.idDevedor,
+            formattedDate
+          );
 
-        const obj = {
-          promessas,
-          ultimaDataVencimento: ultimaData.toISOString().slice(0, 10),
-          vencimentosParcelas: parcelasArray,
-        };
+          const obj = {
+            promessas,
+            ultimaDataVencimento: ultimaData.toISOString().slice(0, 10),
+            vencimentosParcelas: parcelasArray,
+          };
 
-        this._setDataPromessas(phoneNumber, obj);
-        console.log(
-          "userStates -",
-          JSON.stringify(this.userStates, undefined, 2)
-        );
+          this._setDataPromessas(phoneNumber, obj);
+          console.log(
+            "userStates -",
+            JSON.stringify(this.userStates, undefined, 2)
+          );
 
-        const responseDividasCredores = await requests.getCredorDividas(
-          this.idDevedor,
-          ultimaDataFormat
-        );
-
-        const responseDividasCredoresTotais =
-          await requests.getCredorDividasTotais(
+          const responseDividasCredores = await requests.getCredorDividas(
             this.idDevedor,
             ultimaDataFormat
           );
 
-        const {
-          juros_percentual,
-          honorarios_percentual,
-          multa_percentual,
-          tarifa_boleto,
-        } = responseDividasCredoresTotais;
+          const responseDividasCredoresTotais =
+            await requests.getCredorDividasTotais(
+              this.idDevedor,
+              ultimaDataFormat
+            );
 
-        const parsedData = utils.parseDadosAcordo({
-          currentTime,
-          honorarios_percentual,
-          idcredor,
-          iddevedor: this.idDevedor,
-          juros_percentual,
-          multa_percentual,
-          plano,
-          responseDividasCredores,
-          tarifa_boleto,
-          total_geral,
-          ultimaDataVencimento: ultimaDataFormat,
-          parcelasArray,
-        });
+          const {
+            juros_percentual,
+            honorarios_percentual,
+            multa_percentual,
+            tarifa_boleto,
+          } = responseDividasCredoresTotais;
 
-        const idacordo = await requests.postDadosAcordo(parsedData);
-        console.log("idacordo -", idacordo);
-
-        // await this._postMessage(
-        //   origin,
-        //   "Acordo realizado com sucesso - " + JSON.stringify(idacordo)
-        // );
-
-        const parsedData2 = utils.parseDadosPromessa({
-          idacordo,
-          iddevedor: this.idDevedor,
-          plano,
-        });
-
-        let contratos = "";
-        const contratosIncluidos = new Set();
-
-        responseDividasCredores.forEach((dividas, index) => {
-          const { contrato, indice } = dividas;
-
-          // Verifica se o contrato já foi incluído na lista.
-          if (!contratosIncluidos.has(contrato)) {
-            contratos += contrato;
-            contratosIncluidos.add(contrato); // Adiciona o contrato ao Set.
-
-            // Verifica se não é o último contrato antes de adicionar a barra "/".
-            if (index !== responseDividasCredores.length - 1) {
-              contratos += " / ";
-            }
-          }
-        });
-
-        const contratosDividas = contratos;
-
-        const promises = [];
-        let parcelaNumber = 0;
-
-        for await (const parcela of parcelasArray) {
-          parcelaNumber += 1;
-
-          const dataPromessa = {
-            ...parsedData2,
-            data: parcela.vencimento.toISOString().slice(0, 10),
-            valor: parseFloat(parcela.valorParcelaAtual),
-            parcela: parcelaNumber,
-          };
-
-          dataPromessa.mensagem = `Parcela(s) ${parcelaNumber}/${plano} de acordo referente ao(s) título(s): ${contratos}
-    Sr(a). Caixa:
-    Não receber após o vencimento.
-    Não receber valor inferior ao valor facial deste boleto, sem autorização do cedente.
-    Sr (a). Cliente:
-    A utilização deste boleto é obrigatória para adequada confirmação do pagamento.
-    Depósito na conta corrente, sem a devida autorização do cedente, não garante a quitação do débito.
-    `;
-
-          // console.log(dataPromessa.mensagem);
-
-          const promise = await requests.postDadosPromessa(dataPromessa);
-          promises.push(promise);
-        }
-
-        const responsePromessas = await Promise.all(promises);
-        // await this._postMessage(
-        //   origin,
-        //   "Promessas realizadas com sucesso - " +
-        //     JSON.stringify(responsePromessas)
-        // );
-
-        const [ultimoIdPromessa] = responsePromessas.slice(-1);
-
-        const { chave, empresa } = credorInfo[0];
-        const { percentual_comissao_cobrador, idoperacao, idempresa } =
-          responseDividasCredores[0];
-
-        const parsedData3 = utils.parseDadosRecibo({
-          comissao_comercial,
-          cpfcnpj: this.document,
-          honorarios_percentual,
-          idacordo,
-          iddevedor,
-          idcredor,
-          idcomercial,
-          idgerente_comercial,
-          juros_percentual,
-          plano,
-          ultimaDataVencimento: ultimaDataFormat,
-          ultimoIdPromessa,
-          chave,
-          empresa,
-          percentual_comissao_cobrador,
-          idoperacao,
-          idempresa,
-        });
-
-        const responseRecibo = await requests.postDadosRecibo(parsedData3);
-
-        if (
-          responseRecibo &&
-          Object.prototype.hasOwnProperty.call(responseRecibo, "error")
-        ) {
-          console.error(responseRecibo.error);
-          setErrorMessage("Erro ao receber responseRecibo.");
-          return;
-        }
-
-        // await this._postMessage(origin, "Recibo inserido com sucesso!");
-
-        await requests.getAtualizarPromessas(idacordo);
-        await requests.getAtualizarValores(idacordo);
-
-        const responseBoleto = await requests.postBoletoFinal(
-          credorInfo,
-          idacordo,
-          contratosDividas,
-          this.idDevedor,
-          idcredor,
-          plano,
-          total_geral,
-          valor_parcela,
-          comissao_comercial,
-          idcomercial,
-          idgerente_comercial,
-          tarifa_boleto
-        );
-
-        this._setDataBoleto(phoneNumber, responseBoleto);
-
-        const responseIdBoleto = await requests.getIdBoleto(idacordo);
-        console.log("responseIdBoleto -", responseIdBoleto);
-
-        // await this._postMessage(
-        //   origin,
-        //   "getAtualizarPromessas, getAtualizarValores e postDadosBoleto realizado com sucesso!" +
-        //     JSON.stringify(responseBoleto, undefined, 2)
-        // );
-
-        const { idboleto } = responseIdBoleto[0];
-        const { banco } = responseIdBoleto[0];
-        const { convenio } = responseIdBoleto[0];
-
-        console.log(
-          `IdBoleto de número ${idboleto} no banco ${banco} encontrado!`
-        );
-
-        const updateValoresBoleto = await requests.postAtualizarValores({
-          idboleto,
-          banco,
-          convenio,
-        });
-
-        if (
-          updateValoresBoleto &&
-          Object.prototype.hasOwnProperty.call(updateValoresBoleto, "error")
-        ) {
-          console.error("Erro ao atualizar valores de nossoNum e numDoc: ", {
-            updateValoresBoleto,
+          const parsedData = utils.parseDadosAcordo({
+            currentTime,
+            honorarios_percentual,
+            idcredor,
+            iddevedor: this.idDevedor,
+            juros_percentual,
+            multa_percentual,
+            plano,
+            responseDividasCredores,
+            tarifa_boleto,
+            total_geral,
+            ultimaDataVencimento: ultimaDataFormat,
+            parcelasArray,
           });
-          setErro("Erro ao atualizar valores de nossoNum e numDoc.");
-          return;
+
+          const idacordo = await requests.postDadosAcordo(parsedData);
+          console.log("idacordo -", idacordo);
+
+          // await this._postMessage(
+          //   origin,
+          //   "Acordo realizado com sucesso - " + JSON.stringify(idacordo)
+          // );
+
+          const parsedData2 = utils.parseDadosPromessa({
+            idacordo,
+            iddevedor: this.idDevedor,
+            plano,
+          });
+
+          let contratos = "";
+          const contratosIncluidos = new Set();
+
+          responseDividasCredores.forEach((dividas, index) => {
+            const { contrato, indice } = dividas;
+
+            // Verifica se o contrato já foi incluído na lista.
+            if (!contratosIncluidos.has(contrato)) {
+              contratos += contrato;
+              contratosIncluidos.add(contrato); // Adiciona o contrato ao Set.
+
+              // Verifica se não é o último contrato antes de adicionar a barra "/".
+              if (index !== responseDividasCredores.length - 1) {
+                contratos += " / ";
+              }
+            }
+          });
+
+          const contratosDividas = contratos;
+
+          const promises = [];
+          let parcelaNumber = 0;
+
+          for await (const parcela of parcelasArray) {
+            parcelaNumber += 1;
+
+            const dataPromessa = {
+              ...parsedData2,
+              data: parcela.vencimento.toISOString().slice(0, 10),
+              valor: parseFloat(parcela.valorParcelaAtual),
+              parcela: parcelaNumber,
+            };
+
+            dataPromessa.mensagem = `Parcela(s) ${parcelaNumber}/${plano} de acordo referente ao(s) título(s): ${contratos}
+      Sr(a). Caixa:
+      Não receber após o vencimento.
+      Não receber valor inferior ao valor facial deste boleto, sem autorização do cedente.
+      Sr (a). Cliente:
+      A utilização deste boleto é obrigatória para adequada confirmação do pagamento.
+      Depósito na conta corrente, sem a devida autorização do cedente, não garante a quitação do débito.
+      `;
+
+            // console.log(dataPromessa.mensagem);
+
+            const promise = await requests.postDadosPromessa(dataPromessa);
+            promises.push(promise);
+          }
+
+          const responsePromessas = await Promise.all(promises);
+          // await this._postMessage(
+          //   origin,
+          //   "Promessas realizadas com sucesso - " +
+          //     JSON.stringify(responsePromessas)
+          // );
+
+          const [ultimoIdPromessa] = responsePromessas.slice(-1);
+
+          const { chave, empresa } = credorInfo[0];
+          const { percentual_comissao_cobrador, idoperacao, idempresa } =
+            responseDividasCredores[0];
+
+          const parsedData3 = utils.parseDadosRecibo({
+            comissao_comercial,
+            cpfcnpj: this.document,
+            honorarios_percentual,
+            idacordo,
+            iddevedor,
+            idcredor,
+            idcomercial,
+            idgerente_comercial,
+            juros_percentual,
+            plano,
+            ultimaDataVencimento: ultimaDataFormat,
+            ultimoIdPromessa,
+            chave,
+            empresa,
+            percentual_comissao_cobrador,
+            idoperacao,
+            idempresa,
+          });
+
+          const responseRecibo = await requests.postDadosRecibo(parsedData3);
+
+          if (
+            responseRecibo &&
+            Object.prototype.hasOwnProperty.call(responseRecibo, "error")
+          ) {
+            console.error(responseRecibo.error);
+            setErrorMessage("Erro ao receber responseRecibo.");
+            return;
+          }
+
+          // await this._postMessage(origin, "Recibo inserido com sucesso!");
+
+          await requests.getAtualizarPromessas(idacordo);
+          await requests.getAtualizarValores(idacordo);
+
+          const responseBoleto = await requests.postBoletoFinal(
+            credorInfo,
+            idacordo,
+            contratosDividas,
+            this.idDevedor,
+            idcredor,
+            plano,
+            total_geral,
+            valor_parcela,
+            comissao_comercial,
+            idcomercial,
+            idgerente_comercial,
+            tarifa_boleto
+          );
+
+          this._setDataBoleto(phoneNumber, responseBoleto);
+
+          const responseIdBoleto = await requests.getIdBoleto(idacordo);
+          console.log("responseIdBoleto -", responseIdBoleto);
+
+          // await this._postMessage(
+          //   origin,
+          //   "getAtualizarPromessas, getAtualizarValores e postDadosBoleto realizado com sucesso!" +
+          //     JSON.stringify(responseBoleto, undefined, 2)
+          // );
+
+          const { idboleto } = responseIdBoleto[0];
+          const { banco } = responseIdBoleto[0];
+          const { convenio } = responseIdBoleto[0];
+
+          console.log(
+            `IdBoleto de número ${idboleto} no banco ${banco} encontrado!`
+          );
+
+          const updateValoresBoleto = await requests.postAtualizarValores({
+            idboleto,
+            banco,
+            convenio,
+          });
+
+          if (
+            updateValoresBoleto &&
+            Object.prototype.hasOwnProperty.call(updateValoresBoleto, "error")
+          ) {
+            console.error("Erro ao atualizar valores de nossoNum e numDoc: ", {
+              updateValoresBoleto,
+            });
+            setErro("Erro ao atualizar valores de nossoNum e numDoc.");
+            return;
+          }
+
+          const parsedData4 = utils.parseDadosImagemBoleto({
+            idacordo,
+            idboleto,
+            banco,
+          });
+
+          const responseBoletoContent = await requests.getImagemBoleto(
+            parsedData4
+          );
+
+          const parsedData5 = utils.parseDadosImagemQrCode({ idboleto });
+
+          const responseQrcodeContent = await requests.getImagemQrCode(
+            parsedData5
+          );
+          console.log(responseQrcodeContent.url);
+
+          const parsedData6 = utils.parseDadosEmv({ idboleto });
+
+          const responseEmvContent = await requests.getDataEmv(parsedData6);
+
+          await utils.saveQRCodeImageToLocal(responseQrcodeContent.url);
+          const media = MessageMedia.fromFilePath("qrcode.png");
+
+          const mensagem = `*ACORDO REALIZADO COM SUCESSO!*\n\nPague a primeira parcela através do QRCODE ou link do BOLETO abaixo:\n\nhttp://cobrance.com.br/acordo/boleto.php?idboleto=${responseBoletoContent.idboleto}&email=2`;
+
+          await this._postMessage(origin, `MENSAGEM TESTE`);
+
+          await this._postMessage(origin, mensagem);
+          await this._postMessage(origin, media);
+          await this._handleInitialState(origin, phoneNumber, response);
+        } else {
+          await this._postMessage(
+            origin,
+            "Opção inválida. Por favor, escolha uma opção válida."
+          );
         }
-
-        const parsedData4 = utils.parseDadosImagemBoleto({
-          idacordo,
-          idboleto,
-          banco,
-        });
-
-        const responseBoletoContent = await requests.getImagemBoleto(
-          parsedData4
-        );
-
-        const parsedData5 = utils.parseDadosImagemQrCode({ idboleto });
-
-        const responseQrcodeContent = await requests.getImagemQrCode(
-          parsedData5
-        );
-        console.log(responseQrcodeContent.url);
-
-        const parsedData6 = utils.parseDadosEmv({ idboleto });
-
-        const responseEmvContent = await requests.getDataEmv(parsedData6);
-
-        await utils.saveQRCodeImageToLocal(responseQrcodeContent.url);
-        const media = MessageMedia.fromFilePath("qrcode.png");
-
-        const mensagem = `*ACORDO REALIZADO COM SUCESSO!*\n\nPague a primeira parcela através do QRCODE ou link do BOLETO abaixo:\n\nhttp://cobrance.com.br/acordo/boleto.php?idboleto=${responseBoletoContent.idboleto}&email=2`;
-
-        await this._postMessage(origin, `MENSAGEM TESTE`);
-
-        // await this._postMessage(origin, mensagem);
-        // await this._postMessage(origin, media);
-        // await this._handleInitialState(origin, phoneNumber, response);
       } else {
         await this._postMessage(
           origin,
-          "Opção inválida. Por favor, escolha uma opção válida."
+          "Resposta inválida. Por favor, escolha uma opção válida."
         );
       }
-    } else {
-      await this._postMessage(
-        origin,
-        "Resposta inválida. Por favor, escolha uma opção válida."
-      );
+    } catch (error) {
+      console.error("Erro ao lidar com o estado de oferta:", error);
     }
   }
 
