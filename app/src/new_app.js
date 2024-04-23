0eef7ac8da93ba4e294c802160e0d3e64497cbd1
Kevin Medeiros <kevin.medeiros91@gmail.com>
2024-04-23 15:09:44 -0300

    wip

--- app/src/new_app.js
+++ app/src/new_app.js
@@ -108,7 +108,7 @@ client.on("message", async (message) => {
       console.log("Usuario em atendimento humano -", bot_idstatus);
 
       await stateMachine._postMessage(
-        from,
+        fromPhoneNumber,
         `Voce está sendo redirecionado para um atendente humano, por favor aguarde...`
       );
       return;
