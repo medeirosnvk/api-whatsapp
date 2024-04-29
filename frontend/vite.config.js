712db67c5ecec239a618d3243af7267054e6d29d
Kevin Medeiros <kevin.medeiros91@gmail.com>
2024-04-29 15:30:13 -0300

    vite config hmr

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,17 @@
 import { defineConfig } from "vite";
+import { sveltekit } from "@sveltejs/kit/vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sveltekit()],
   server: {
     port: 5174,
     host: "0.0.0.0", // Isso permitirá que o servidor seja acessível a partir de qualquer IP externo
 
     // Configurações do servidor HMR (WebSocket)
     hmr: {
-      protocol: "ws", // ou 'wss' se estiver usando HTTPS
-      host: "191.101.70.186", // Altere para o endereço do seu servidor WebSocket
-      port: 5174, // Altere para a porta desejada
-      path: "/websocket", // Caminho para a rota WebSocket
+      clientPort: 5174,
     },
   },
 });
