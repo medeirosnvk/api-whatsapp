55a6268ce71f43134e3593311758dfa6191ea42e
Kevin Medeiros <kevin.medeiros91@gmail.com>
2024-04-29 14:26:46 -0300

    wip

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,15 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5174,
-    proxy: {
-      "/ws": {
-        target: "//191.101.70.186", // Altere para o endereço do seu servidor WebSocket
-        changeOrigin: true,
-        ws: true,
-      },
-    },
     host: "0.0.0.0", // Isso permitirá que o servidor seja acessível a partir de qualquer IP externo
-    port: 5174, // Altere para a porta desejada
 
     // Configurações do servidor HMR (WebSocket)
     hmr: {
