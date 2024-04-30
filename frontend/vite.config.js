b94ef8e0797c45225de1289c02016bbcf97d7a21
Kevin Medeiros <kevin.medeiros91@gmail.com>
2024-04-30 13:22:36 -0300

    wip

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { sveltekit } from "@sveltejs/kit/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sveltekit()],
+  plugins: [react()],
   server: {
     port: 5174,
-    hmr: {
-      clientPort: 5174,
-    },
   },
 });
