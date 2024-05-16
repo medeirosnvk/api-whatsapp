003e98ec6c9f80b5a7ed736dbba3d874ebb4b644
Kevin Medeiros <kevin.medeiros91@gmail.com>
2024-05-16 16:19:20 -0300

    try cath conexao banco

--- app/src/dbconfig.js
+++ app/src/dbconfig.js
@@ -2,7 +2,6 @@ const puppeteer = require("puppeteer");
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-// Função para criar e retornar uma nova instância do navegador com a flag --no-sandbox
 const createBrowserInstance = async () => {
   const browser = await puppeteer.launch({
     headless: false,
@@ -13,13 +12,11 @@ const createBrowserInstance = async () => {
   return browser;
 };
 
-// Função para criar e retornar uma nova conexão com o banco de dados
 const createConnection = async (dbConfig) => {
   const connection = await mysql.createConnection(dbConfig);
   return connection;
 };
 
-// Configuração padrão da conexão MySQL usando variáveis de ambiente
 const defaultDbConfig = {
   host: process.env.DB1_MY_SQL_HOST,
   user: process.env.MY_SQL_USER,
@@ -30,14 +27,18 @@ const defaultDbConfig = {
   charset: process.env.MY_SQL_CHARSET,
 };
 
-// Função para executar consultas no banco de dados
 const executeQuery = async (sql, customDbConfig = defaultDbConfig) => {
-  const connection = await createConnection(customDbConfig);
   try {
-    const [rows, fields] = await connection.execute(sql);
-    return rows;
-  } finally {
-    await connection.end();
+    const connection = await createConnection(customDbConfig);
+    try {
+      const [rows, fields] = await connection.execute(sql);
+      return rows;
+    } finally {
+      await connection.end();
+    }
+  } catch (error) {
+    console.error("Erro ao executar a consulta:", error);
+    throw error; // Re-throw the error to handle it in the caller function
   }
 };
 
