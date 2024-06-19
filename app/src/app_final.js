4db26aef9a34209192b964cbb28292c6885c6960
Kevin Medeiros <kevin.medeiros91@gmail.com>
2024-06-19 15:28:35 -0300

    wip

--- app/src/app_final.js
+++ app/src/app_final.js
@@ -1261,9 +1261,38 @@ const disconnectSession = async (sessionName) => {
   if (client) {
     try {
       await client.logout();
-      console.log(`Session ${sessionName} disconnected`);
+      console.log(`Sessão ${sessionName} desconectada`);
+
+      const sessionPath = path.join(
+        __dirname,
+        "../.wwebjs_auth",
+        `session-${sessionName}`
+      );
+
+      // Função para excluir a pasta da sessão
+      const deleteFolderRecursive = (folderPath) => {
+        if (fs.existsSync(folderPath)) {
+          fs.readdirSync(folderPath).forEach((file) => {
+            const curPath = path.join(folderPath, file);
+            if (fs.lstatSync(curPath).isDirectory()) {
+              // Recursivamente exclui pastas
+              deleteFolderRecursive(curPath);
+            } else {
+              // Exclui arquivos
+              fs.unlinkSync(curPath);
+            }
+          });
+          fs.rmdirSync(folderPath);
+          console.log(
+            `Diretório de autenticação da sessão ${sessionName} excluído com sucesso!`
+          );
+        }
+      };
+
+      // Excluir a pasta da sessão
+      deleteFolderRecursive(sessionPath);
     } catch (error) {
-      console.error(`Error disconnecting session ${sessionName}:`, error);
+      console.error(`Erro ao desconectar a sessão ${sessionName}:`, error);
       throw error;
     }
   }
